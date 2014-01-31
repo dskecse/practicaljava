28723a515ca5e5a75414350d1c0fbc049da9a218
Dzianis Dashkevich <dskecse@gmail.com>
2014-02-01 00:53:35 +0300

    Add an Address Printer cli program

---
+++ AddressPrinter/src/com/practicaljava/l2/Address.java
@@ -0,0 +1,14 @@
+package com.practicaljava.l2;
+
+public class Address {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("Dzianis Dashkevich");
+    System.out.println("123 Ocean Ave");
+    System.out.println("Miami, FL 10001");
+    System.out.println("USA");
+  }
+}
