8be5e01a3643720cb76461ceed83fb185c8c93b1
Dzianis Dashkevich <dskecse@gmail.com>
2014-02-01 08:36:52 +0300

    Correct line endings

--- Sale/src/com/practicaljava/l2/FriendsAndFamily.java
+++ Sale/src/com/practicaljava/l2/FriendsAndFamily.java
@@ -11,13 +11,14 @@ public class FriendsAndFamily {
     Scanner input = new Scanner(System.in);
 
     do {
-      System.out.println("\n\nEnter list price:");
+      System.out.println("Enter list price:");
       double listPrice = input.nextDouble();
  
       System.out.println("Enter discount %:");
       int discount = input.nextInt();
       
-      System.out.printf("You'll pay only %2.2f", listPrice * (1 - discount / 100.0));
+      System.out.printf("You'll pay only %2.2f\n\n",
+          listPrice * (1 - discount / 100.0));
     } while (true);
   }
 }
