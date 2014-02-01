c8e7a2efca09657476e59a96c9b385921e339332
Dzianis Dashkevich <dskecse@gmail.com>
2014-02-01 15:10:08 +0300

    Add code needs refactoring

--- unit1/FirstEclipseProject/src/com/practicaljava/l2/Hello.java
+++ unit1/FirstEclipseProject/src/com/practicaljava/l2/Hello.java
@@ -7,5 +7,10 @@ public class Hello {
    */
   public static void main(String[] args) {
     System.out.println("Hello, World, from inside Eclipse IDE!!!");
+    int sum = 0;
+    for (int i = 1; i <= 100; i++) {
+      sum += i;
+    }
+    System.out.println(sum);
   }
 }
