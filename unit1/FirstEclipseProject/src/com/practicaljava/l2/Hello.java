6a1fb589d4b387f1db88d4e031300e397817c4a5
Dzianis Dashkevich <dskecse@gmail.com>
2014-02-01 15:16:08 +0300

    Do refactoring

--- unit1/FirstEclipseProject/src/com/practicaljava/l2/Hello.java
+++ unit1/FirstEclipseProject/src/com/practicaljava/l2/Hello.java
@@ -2,15 +2,22 @@ package com.practicaljava.l2;
 
 public class Hello {
 
+  private static final String HELLO = "Hello, World, from inside Eclipse IDE!!!";
+
   /**
    * @param args
    */
   public static void main(String[] args) {
-    System.out.println("Hello, World, from inside Eclipse IDE!!!");
+    System.out.println(HELLO);
     int sum = 0;
+    sum = calculateSum(sum);
+    System.out.println(sum);
+  }
+
+  private static int calculateSum(int sum) {
     for (int i = 1; i <= 100; i++) {
       sum += i;
     }
-    System.out.println(sum);
+    return sum;
   }
 }
