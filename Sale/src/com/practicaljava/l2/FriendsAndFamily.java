e6376aeeeab2c91eed3ae78a0578cb86ec1ea00c
Dzianis Dashkevich <dskecse@gmail.com>
2014-02-01 08:28:02 +0300

    Add another example of Java cli program made in Eclipse

---
+++ Sale/src/com/practicaljava/l2/FriendsAndFamily.java
@@ -0,0 +1,23 @@
+package com.practicaljava.l2;
+
+import java.util.Scanner;
+
+public class FriendsAndFamily {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    do {
+      System.out.println("\n\nEnter list price:");
+      double listPrice = input.nextDouble();
+ 
+      System.out.println("Enter discount %:");
+      int discount = input.nextInt();
+      
+      System.out.printf("You'll pay only %2.2f", listPrice * (1 - discount / 100.0));
+    } while (true);
+  }
+}
