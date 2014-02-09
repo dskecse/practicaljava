528136d8f5154af44315bb92c190cb2815fa4616
Dzianis Dashkevich <dskecse@gmail.com>
2014-02-09 18:49:17 +0300

    Add James Bond's car for testing

--- unit2/OOP/src/com/practicaljava/l3/TestCar.java
+++ unit2/OOP/src/com/practicaljava/l3/TestCar.java
@@ -8,14 +8,19 @@ public class TestCar {
   public static void main(String[] args) {
     Car car1 = new Car();
     Car car2 = new Car();
+    JamesBondCar car3 = new JamesBondCar();
 
     car1.color = "blue";
     car2.color = "red";
+    car3.color = "rifle-green";
 
     System.out.println("I've painted the cars!!");
     System.out.println("The color of 1st car is " + car1.color);
     System.out.println("The color of 2nd car is " + car2.color);
+    System.out.println("The color of James Bond's car is " + car3.color);
 
     car1.startEngine();
+    car3.startEngine();
+    car3.stopEngine();
   }
 }
