3af2bd4716ec08162563c52b6078e3300abaac42
Dzianis Dashkevich <dskecse@gmail.com>
2014-02-09 20:39:01 +0300

    Override calcTax() method for NJ

--- unit2/OOP/src/com/practicaljava/l3/NJTax.java
+++ unit2/OOP/src/com/practicaljava/l3/NJTax.java
@@ -1,6 +1,14 @@
 package com.practicaljava.l3;
 
 public class NJTax extends Tax {
+  public double calcTax() {
+    if (grossIncome < 50000) {
+      return grossIncome * 0.1;
+    } else {
+      return grossIncome * 0.13;
+    }
+  }
+
   public double adjustForStudents(double tax) {
     return tax - 500;
   }
--- unit2/OOP/src/com/practicaljava/l3/TestTax.java
+++ unit2/OOP/src/com/practicaljava/l3/TestTax.java
@@ -6,14 +6,21 @@ public class TestTax {
    * @param args
    */
   public static void main(String[] args) {
-    NJTax tax = new NJTax(); // special for NJ
+    Tax tax1 = new Tax();
+    NJTax tax2 = new NJTax(); // special for NJ
 
-    tax.grossIncome = 50000.45;
-    tax.dependents = 2;
-    tax.state = "NJ";
+    tax1.grossIncome = 50000;
+    tax1.dependents = 2;
+    tax1.state = "NY";
 
-    double yourTax = tax.calcTax();
+    tax2.grossIncome = 50000;
+    tax2.dependents = 2;
+    tax2.state = "NJ";
 
-    System.out.printf("Your tax is %.2f", tax.adjustForStudents(yourTax));
+    System.out.printf("Your tax is %.2f\n", tax1.calcTax());
+
+    double yourNJTax = tax2.calcTax();
+    System.out.printf("Your tax is %.2f in NJ",
+        tax2.adjustForStudents(yourNJTax));
   }
 }
